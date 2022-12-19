2fb36a9477ad0baff66c81cbde08da0f8ab312ba
Eric Separovic <ericseparovic07@gmail.com>
2022-12-19 12:01:02 -0300

    Create styles Fromulario components

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,8 +1,80 @@
 function Formulario() {
   return (
-    <>
-      <h2>Formulario</h2>
-    </>
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+      <p className="text-lg mt-5 text-center">
+        Agrega Pacientes y{" "}
+        <span className="text-indigo-600 font-bold mb-10">Administralos</span>
+      </p>
+
+      <form className="bg-white shadow-md rounded-lg py-10 px-5">
+        <div className="mb-5">
+          <label
+            className="block text-gray-700 uppercase font-bold"
+            htmlFor="mascota"
+          >
+            Nombre mascota
+          </label>
+          <input
+            type="text"
+            placeholder="Nombre de la mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            id="mascota"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            className="block text-gray-700 uppercase font-bold"
+            htmlFor="propietario"
+          >
+            Nombre Propietario
+          </label>
+          <input
+            type="text"
+            placeholder="Nombre del Propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            id="propietario"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            className="block text-gray-700 uppercase font-bold"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            placeholder="Ingrese un email"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            id="email"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            className="block text-gray-700 uppercase font-bold"
+            htmlFor="alta"
+          >
+            Alta
+          </label>
+          <input
+            type="date"
+            placeholder="Ingrese un email"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            id="alta"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            className="block text-gray-700 uppercase font-bold"
+            htmlFor="sintomas"
+          >
+            Sintomas
+          </label>
+          <textarea id="sintomas" placeholder="Describe los sitomas" />
+        </div>
+      </form>
+    </div>
   );
 }
 
